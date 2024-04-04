cd4344e1ca4354a10e5eae49b0b10dbd833a70c0
Powerbuilder_1 <philipp.120201@gmail.com>
2024-04-04 16:45:40 +0200

    implement fetch handler and create_pdu_from_pdu_wrapper + extract_data_from_pdu (insert into pdu wrapper) func.

--- services/listen_dnsport.h
+++ services/listen_dnsport.h
@@ -151,6 +151,8 @@ struct listen_port {
 
 };
 
+void free_pdu_response_data(struct pdu_response_data* data);
+
 /**
  * Create shared listening ports
  * Getaddrinfo, create socket, bind and listen to zero or more 
@@ -374,6 +376,8 @@ struct tcp_req_done_item {
 	/** length of the buffer */
 	size_t len;
 };
+
+void extract_pdu_info(const coap_pdu_t *pdu, struct pdu_response_data *data);
 
 /**
  * Create tcp request info structure that keeps track of open
